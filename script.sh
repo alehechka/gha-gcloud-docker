96ffd7add042f6beaf6ddd27625d306b50f2dd35
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2021-12-30 21:42:33 -0600

    access secret

--- script.sh
+++ script.sh
@@ -14,3 +14,5 @@ echo "$GCP_SA_KEY" > /tmp/certs/svc_account.json
 gcloud auth activate-service-account --key-file=/tmp/certs/svc_account.json --project $(cat /tmp/certs/svc_account.json | jq -r '.project_id') --no-user-output-enabled
 
 gcloud secrets list
+
+gcloud secrets versions access latest --secret="DEVELOP_KEY" --format='get(payload.data)' | tr '_-' '/+' | base64 -d
