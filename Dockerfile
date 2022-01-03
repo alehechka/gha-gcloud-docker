b239ad2a6788b8978b0e780edfba1ee52cb879fa
Adam Lehechka <ajlehechka@gmail.com>
2022-01-03 11:16:59 -0600

    reformat bash script

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ FROM google/cloud-sdk:alpine
 RUN apk --update add jq
 
 COPY secret_manager.sh /secret_manager.sh
-COPY env_prefixer.sh /env_prefixer.sh
 
 ENTRYPOINT ["/secret_manager.sh"]
--- secret_manager.sh
+++ secret_manager.sh
@@ -1,31 +1,67 @@
 #!/bin/bash
 
-GCP_SA_KEY=$1
-PROJECT_ID=$(echo "$GCP_SA_KEY" | jq -r '.project_id')
-KEYS=$2
-PREFIX=$(./env_prefixer.sh "$GITHUB_ACTION_REF" "$GITHUB_REF_TYPE")
-echo "$PREFIX"
+function main() {
+    GCP_SA_KEY=$1
+    KEYS=$2
 
-echo "Authenticating Service Account with gcloud..."
-mkdir -p /tmp/certs
-echo "$GCP_SA_KEY" > /tmp/certs/svc_account.json
-gcloud auth activate-service-account --key-file=/tmp/certs/svc_account.json --project "$PROJECT_ID" --no-user-output-enabled
+    env | sort
 
-echo "Retrieving secrets from Secret Manager..."
-for KEY in ${KEYS//,/ }
-do
-    echo "Retrieving secret for: $KEY"
+    gcloud_auth "$GCP_SA_KEY" 
 
-    # https://cloud.google.com/secret-manager/docs/creating-and-accessing-secrets#access
-    SECRET=$(gcloud secrets versions access latest --secret="$PREFIX$KEY" --format='get(payload.data)' | tr '_-' '/+' | base64 -d)
+    get_secrets "$KEYS"
+}
 
-    if [ -z "$SECRET" ]; then
-        exit 1
+function gcloud_auth() {
+    GCP_SA_KEY=$1
+    PROJECT_ID=$(echo "$GCP_SA_KEY" | jq -r '.project_id')
+
+    echo "Authenticating Service Account with gcloud..."
+    mkdir -p /tmp/certs
+    echo "$GCP_SA_KEY" > /tmp/certs/svc_account.json
+    gcloud auth activate-service-account --key-file=/tmp/certs/svc_account.json --project "$PROJECT_ID" --no-user-output-enabled
+}
+
+function get_secrets() {
+    KEYS=$1
+    PREFIX=$(env_prefixer "$GITHUB_ACTION_REF" "$GITHUB_REF_TYPE")
+    echo "$PREFIX"
+
+    echo "Retrieving secrets from Secret Manager..."
+    for KEY in ${KEYS//,/ }
+    do
+        echo "Retrieving secret for: $KEY"
+
+        # https://cloud.google.com/secret-manager/docs/creating-and-accessing-secrets#access
+        SECRET=$(gcloud secrets versions access latest --secret="$PREFIX$KEY" --format='get(payload.data)' | tr '_-' '/+' | base64 -d)
+
+        if [ -z "$SECRET" ]; then
+            exit 1
+        fi
+
+        # echo "::add-mask::$SECRET"
+
+        echo "::set-output name=$KEY::$SECRET"
+    done
+}
+
+function env_prefixer() {
+    REF=$1
+    REF_TYPE=$2
+
+    PROD="PROD_"
+    DEVELOP="DEVELOP_"
+
+    # https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+    # Semver official doesn't work with Bash, used this modified version: https://gist.github.com/rverst/1f0b97da3cbeb7d93f4986df6e8e5695#gistcomment-3029858
+    SEMVER_REGEX="^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-((0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?$"
+
+    if [ "$REF" = 'main' ]; then
+        echo "$PROD"
+    elif [[ "$REF_TYPE" = 'tag' ]] && [[ "$REF" =~ $SEMVER_REGEX ]]; then
+        echo "TAG"
+    else
+        echo "$DEVELOP"
     fi
+}
 
-    # echo "::add-mask::$SECRET"
-
-    echo "::set-output name=$KEY::$SECRET"
-done
-
-env | sort
+main "$@"; exit
